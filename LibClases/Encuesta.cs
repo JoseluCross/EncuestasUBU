a1b116c85f5f8734d878079e2920bcf6c110dd35
JoseluCross <joselucross@jkanetwork.com>
2018-10-03 20:28:30 +0200

    Comentarios

--- LibClases/Encuesta.cs
+++ LibClases/Encuesta.cs
@@ -7,54 +7,92 @@ namespace LibClases
 {
     class Encuesta
     {
+        /// <summary>
+        /// Título de la encuesta y clave de la misma
+        /// </summary>
         private string titulo;
+        /// <summary>
+        /// Descripción de la encuesta
+        /// </summary>
         private string descripcion;
+        /// <summary>
+        /// Ruta de la Foto para mostrarla en el HTML
+        /// </summary>
         private string rutaFoto;
+        /// <summary>
+        /// Encuesta visible o no visible
+        /// </summary>
         private bool visible;
 
-        public Encuesta(string titulo)
-        {
-            this.titulo = titulo;
-            ///TODO: Read from database
-        }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="titulo"></param>
+        /// <param name="descripcion"></param>
+        /// <param name="rutaFoto"></param>
+        /// <param name="visible"></param>
         public Encuesta(string titulo, string descripcion, string rutaFoto, bool visible = false)
         {
             this.titulo = titulo;
             this.descripcion = descripcion;
             this.rutaFoto = rutaFoto;
             this.visible = visible;
-            ///TODO: Almacenar en la base de datos
         }
 
-        public void actualizar()
+        /// <summary>
+        /// Almacena la encuesta en la base de datos como si fuera nueva
+        /// </summary>
+        public void almacenar()
+        {
+            ///TODO: Almacenar los cambios en la base de datos.
+        }
+
+        /// <summary>
+        /// Actualiza los datos en la base de datos
+        /// </summary>
+        private void actualizar(string tituloViejo = null)
         {
             ///TODO: Almacenar los cambios en al base de datos.
         }
 
+        /// <summary>
+        /// Cambiar visilidad a la encuesta a la contraria que tenía
+        /// </summary>
         public void cambiarVisibilidad()
         {
             this.visible = !visible;
         }
 
+        /// <summary>
+        /// Getter y setter del título
+        /// </summary>
         public string Titulo
         {
             get { return this.titulo; }
             set { this.titulo = value; }
         }
 
+        /// <summary>
+        /// Getter y setter de la descripción
+        /// </summary>
         public string Descripcion
         {
             get { return this.descripcion; }
             set { this.descripcion = value; }
         }
 
+        /// <summary>
+        /// Getter y setter de la ruta de la foto
+        /// </summary>
         public string RutaFoto
         {
             get { return this.rutaFoto; }
             set { this.rutaFoto = value; }
         }
 
+        /// <summary>
+        /// Getter de la visibilidad
+        /// </summary>
         public bool Visible
         {
             get { return this.visible; }
--- LibClases/Respuesta.cs
+++ LibClases/Respuesta.cs
@@ -7,32 +7,52 @@ namespace LibClases
 {
     class Respuesta
     {
+        /// <summary>
+        /// Encuesta a la que pertenece la respuesta
+        /// </summary>
         private Encuesta enc;
+        /// <summary>
+        /// Voto dado
+        /// </summary>
         private Voto voto;
-        private string mensage;
+        /// <summary>
+        /// Mensaje 
+        /// </summary>
+        private string mensaje;
 
-        public Respuesta(Encuesta enc, Voto voto, string mensage)
+        /// <summary>
+        /// Crea una respuesta
+        /// </summary>
+        /// <param name="enc">Encuesta de la respuesta</param>
+        /// <param name="voto">Voto dado</param>
+        /// <param name="mensaje">Mensaje dado</param>
+        public Respuesta(Encuesta enc, Voto voto, string mensaje)
         {
             this.enc = enc;
             this.voto = voto;
-            this.mensage = mensage;
+            this.mensaje = mensaje;
             ///TODO: Almacenar en la base de datos.
         }
 
-        public Respuesta(Encuesta enc, int index)
-        {
-            this.enc = enc;
-            ///TODO: Cargar encuesta con el índice en la base de datos
-        }
-
+        /// <summary>
+        /// Getter del voto 
+        /// </summary>
         public Voto Voto
         {
             get { return this.voto; }
         }
 
-        public string Mensage
+        /// <summary>
+        /// Getter del mensaje
+        /// </summary>
+        public string Mensaje
         {
-            get { return this.mensage; }
+            get { return this.mensaje; }
+        }
+
+        public Encuesta Encuesta
+        {
+            get { return this.enc; }
         }
     }
 }
