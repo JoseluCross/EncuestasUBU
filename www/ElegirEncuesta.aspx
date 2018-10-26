1c9732d55a4117192b7230180c426059bf7ccae4
Josemi <JMRS3797@gmail.com>
2018-10-26 13:57:37 +0200

    Imagenes y ElegirEncuesta
    Imagenes y ElegirEncuesta

---
+++ www/ElegirEncuesta.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ElegirEncuesta.aspx.cs" Inherits="www.ElegirEncuesta" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>
+                Encuestas que puede votar:
+            </h1>
+        </div>
+        <asp:Table ID="TEV" runat="server">
+        </asp:Table>
+        <asp:Button ID="BA" runat="server" Text="Administrar" OnClick="BA_Click" />
+    </form>
+</body>
+</html>
---
+++ www/ElegirEncuesta.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LibClases;
+
+namespace www
+{
+    public partial class ElegirEncuesta : System.Web.UI.Page
+    {
+        private DB db;
+        private List<Encuesta> enc;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            db = (DB)Application["db"];
+            if (db is null)
+            {
+                db = DB.getDB();
+                Application["db"] = db;
+            }
+            enc = db.cargaEncuestasVisibles();
+            foreach(Encuesta en in enc)
+            {
+                TableRow tr = new TableRow();
+                TableCell n = new TableCell();
+                TableCell m = new TableCell();
+                ImageButton ib = new ImageButton();
+                ib.ImageUrl = "images/encImg/" + en.RutaFoto;
+                Label l = new Label();
+                l.Text = en.Titulo;
+                ib.ID = en.Titulo;
+                ib.Click += new ImageClickEventHandler(IB_Click);
+                n.Controls.Add(ib);
+                m.Controls.Add(l);
+                tr.Controls.Add(n);
+                tr.Controls.Add(m);
+                TEV.Controls.Add(tr);
+            }
+        }
+
+        protected void BA_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+
+        protected void IB_Click(object sender, EventArgs e)
+        {
+            ImageButton b = (ImageButton)sender;
+            Response.Redirect("Votar.aspx?m=" + b.ID);
+        }
+    }
+}
